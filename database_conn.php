8383b5867db4aa46d8ba0bd83310272d816bef1c
AnonLearnerk <127395976+AnonLearnerk@users.noreply.github.com>
2024-06-02 00:20:16 +0800

    Update database_conn.php

--- database_conn.php
+++ database_conn.php
@@ -1,13 +1,1 @@
-<?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
-
-$db_name = "test_data";
-
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
-
-if (!$conn) {
-	echo "Connection failed!";
-}
